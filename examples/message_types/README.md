9ee8b3bdc851057b791ea36b245808b9f03df517
Victor Gridnevsky <vic.6r1d@gmail.com>
2021-01-28 09:34:50 +0300

    Adds README to a message type checker

---
+++ examples/message_types/README.md
@@ -0,0 +1,7 @@
+# Description
+
+This example demonstrates how `get_midi_msg_type` works by iterating through a number of MIDI message types (`128` - `255`).
+
+# TO-DO
+
+Check if old `name` values are removed. They probably aren't, but I'm not sure about applying `free` to `const char` and GCC complains about it, too.
--- examples/message_types/message_types.c
+++ examples/message_types/message_types.c
@@ -2,13 +2,17 @@
 #include "midi.h"
 
 int main(int argc, char *argv[]) {
-   const char * name;
-   uint8_t value = 128;
-   while (value != 0) {
-      name = get_midi_msg_type(value);
-      printf("%s\n", name);
-      value++;
-   }
-
-   return 0;
+    // Message type string
+    const char * name;
+    // Currently checked message type
+    uint8_t value = 128;
+    // Iterate through all possible messages,
+    // display message types and channels
+    while (value != 0) {
+        name = get_midi_msg_type(value);
+        printf("%s\n", name);
+        value++;
+    }
+    // Exit normally
+    return 0;
 }
